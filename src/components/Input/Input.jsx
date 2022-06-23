5de9454efd334edb7ba94f0ed62e1cfc50526393
rokas <rokasra@gmail.com>
2022-06-23 09:05:26 +0300

    UI buttons FORM input

--- src/components/Input/Input.jsx
+++ src/components/Input/Input.jsx
@@ -1,11 +1,13 @@
 import css from './Input.module.css';
 import PropTypes from 'prop-types';
 
-function Input({ color = 'light' }) {
-  return <input className={`${css.input} ${css[color]}`} />;
+function Input({ color = 'light', placeholder, value }) {
+  return <input className={`${css.input} ${css[color]}`} placeholder={placeholder} value={value} />;
 }
 
 Input.propTypes = {
+  placeholder: PropTypes.string.isRequired,
   color: PropTypes.oneOf(['light', 'dark']),
+  value: PropTypes.string,
 };
 export default Input;
--- src/components/Input/Input.stories.jsx
+++ src/components/Input/Input.stories.jsx
@@ -2,11 +2,23 @@ import React from 'react';
 import Input from './Input';
 
 export default {
-  title: 'Input',
+  title: 'FORM/Input',
   component: Input,
 };
 
 const Template = (args) => <Input {...args} />;
 
 export const MainInput = Template.bind({});
-MainInput.args = {};
+MainInput.args = {
+  placeholder: 'Enter text here',
+};
+export const DarkInput = Template.bind({});
+DarkInput.args = {
+  ...MainInput.args,
+  color: 'dark',
+};
+export const TextArea = Template.bind({});
+TextArea.args = {
+  ...MainInput.args,
+  color: 'dark',
+};
